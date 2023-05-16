bf9fc9c854d632a742c704708559c2e1e45ebcb7
YeLJ <you@example.com>
2023-05-16 00:05:58 -0700

    completed

--- user/sleep.c
+++ user/sleep.c
@@ -5,9 +5,10 @@
 int
 main(int argc, char *argv[])
 {
-    if( argc < 1){
-        char *emsg = "Please input an arugemt to determine sleep time.";
-        write(1,emsg, sizeof emsg);
+    if( argc <= 1){
+        char *emsg = "Please input an arugemt to determine sleep time.\n";
+        // fprintf(2, "arugemt error\n");
+        write(1,emsg, strlen(emsg));
         exit(1);
     }
     int clock_cnt = atoi(argv[1]);
